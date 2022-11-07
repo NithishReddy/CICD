c11a4ab31c603a1dbb5853616672c37cd6748098
Kalva Reddy <kalva.reddy@paytm.com>
2022-11-07 17:26:00 +0530

    changes

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,8 +1,63 @@
 from unittest import TestCase
 import unittest
 from unittest.mock import MagicMock
-from code import Calculator
-from exceptions import InvalidInput, DivisionByZero
+#from code import Calculator
+#from exceptions import InvalidInput, DivisionByZero
+
+
+class InvalidInput(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args)
+        self.message = kwargs.get("message")
+    
+    def __str__(self):
+        return self.message
+    
+class DivisionByZero(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args)
+        self.message = kwargs.get("message")
+    
+    def __str__(self):
+        return self.message
+
+class Calculator:
+    
+    @staticmethod
+    def addition(var1, var2):
+        return var1 + var2
+
+    @staticmethod
+    def subtraction(var1, var2):
+        return var1 - var2
+    
+    @staticmethod
+    def multiplication(var1, var2):
+        return var1 * var2
+    
+    @staticmethod
+    def division(var1, var2):
+        
+        if var2 == 0:
+            raise DivisionByZero(message = 'Division with Zero')
+        
+        return var1/var2
+    
+    @staticmethod
+    def perform_operation(operator, var1, var2):
+        if var1 is None or var2 is None or type(var1) == str or type(var2) == str:
+            raise InvalidInput(message = "please enter valid values")
+
+        if operator == '+':
+            return Calculator.addition(var1, var2)
+        elif operator == '-':
+            return Calculator.subtraction(var1, var2)
+        elif operator == '*':
+            return Calculator.multiplication(var1, var2)
+        elif operator == '/':
+            return Calculator.division(var1, var2)
+        else:
+            raise InvalidInput(message = "Dont support other calculations")
 
 class CalculatorTest(TestCase):
     def setUp(self):
